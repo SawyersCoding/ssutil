6664966f9da3a87432b148541576385f012263a4
Sawyer Stanley <sawyerscoding@gmail.com>
2024-07-21 11:26:07 -0300

    created statuses

---
+++ ssbehaviourtree/src/ssbehtreestatus.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file ssbehtreestatus.hpp
+ *
+ * @brief The status options which a behaviour tree can return.
+ *
+ * @namespace ssutil
+ *
+ * @author Sawyer Stanley (https://github.com/SawyersCoding)
+ */
+
+#ifndef SSIBEHTREESTATUS_HPP
+#define SSIBEHTREESTATUS_HPP
+
+enum ssbehtreestatus{
+    FAILURE = 0,
+    SUCCESS = 1,
+    RUNNING = 2
+};
+
+#endif // SSIBEHTREESTATUS_HPP
