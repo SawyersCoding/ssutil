c431e7d4feb2a88f5ba82538b9cf41b99e53dc87
Sawyer Stanley <sawyerscoding@gmail.com>
2024-07-21 07:59:54 -0300

    Made ssevent.hpp

---
+++ ssevent/src/ssevent.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file ssevent.hpp
+ *
+ * @brief ssevent are used for invoking designated 
+ * functions when an event of interest occurs.
+ *
+ * @namespace ssutil
+ *
+ * @author Sawyer Stanley
+ * Contact: sawyerscoding@gmail.com
+ */
+
+
+#ifndef SSEVENT_HPP
+#define SSEVENT_HPP
+
+#include <functional>
+#include "ssievent.hpp"
+
+namespace ssutil{
+
+    class ssevent : public ssievent {
+
+        public:
+
+            /// @brief Adds the given callback from the set of callbacks to be invoked
+            /// during notify().
+            /// @param callback The callback to add.
+            void add_callback(const std::function<void(void*)> callback) override;
+
+            /// @brief Invokes all callbacks.
+            void notify() override;
+
+            /// @brief Removes the given callback from the set of callbacks to be invoked
+            /// during notify().
+            /// @param callback The callback to remove.
+            void remove_callback(const std::function<void(void*)> callback) override;
+
+    };
+
+}
+
+#endif // SSEVENT_HPP
